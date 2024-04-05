5a169692d168a3ef4ad9a3601341d5721deefab7
CharlesLehnert <Charlielehnert7@gmail.com>
2024-04-05 02:45:59 -0500

    redoing layout

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,94 +1,112 @@
-import React, { useEffect, useState } from "react";
-import { listReservations, listTables } from "../utils/api";
-import ErrorAlert from "../Errors/ErrorAlert";
-import { today, next, previous, formatDate } from "../utils/date-time";
-import Reservation from "../reservations/Reservation";
-// Update the import statement
-import Tables from "../Tables/Tables";
+import React from "react";
+import ErrorAlert from "../layout/ErrorAlert";
+import ReservationRender from "./ReservationRender";
+import TableRender from "./TableRender";
+import { useHistory } from "react-router-dom";
+/**
+ * Defines the dashboard page.
+ * @param date
+ *  the date for which the user wants to view reservations.
+ * @returns {JSX.Element}
+ */
+function Dashboard({ date, reservations, reservationsError, tables, tablesError, loadDashboard }) {
+  
+  const history = useHistory();
 
-import useQuery from "../utils/useQuery";
-
-
-export default function Dashboard() {
-  const query = useQuery();
-  const [reservations, setReservations] = useState([]);
-  const [reservationsError, setReservationsError] = useState(null);
-  const [tables, setTables] = useState([]);
-  const [tablesError, setTablesError] = useState(null);
-  const [date, setDate] = useState(query.get("date") || today());
-
-
-  useEffect(loadDashboard, [date]);
-
-  const handleDateChange = (event) => {
-    setDate(event.target.value);
-  };
-
-
-  function loadDashboard() {
-    const abortController = new AbortController();
-    setReservationsError(null);
-    listReservations({ date }, abortController.signal)
-      .then(setReservations)
-      .catch(setReservationsError);
-    listTables(abortController.signal).then(setTables).catch(setTablesError);
-    return () => abortController.abort();
+  const reservationsRender = () => {
+    return reservations.map((reservation) => (
+      <ReservationRender key={reservation.reservation_id} reservation={reservation} loadDashboard={loadDashboard}/>
+    ))
   }
 
-  
+  const tablesRender = () => {
+    return tables.map((table) => (
+      <TableRender key={table.table_id} table={table} loadDashboard={loadDashboard}/>
+    ))
+  }
+
+  function handleClick({target}){
+    const queryParams = new URLSearchParams(window.location.search)
+    let date = queryParams.get("date");
+    
+    let day = (date) ? new Date(date) : new Date();
+    day.setUTCDate(day.getUTCDate());
+    let dayDate = day.toISOString().slice(0,10);
+
+    if(target.innerHTML === "Next Day"){
+      let nextDay = new Date(dayDate);
+      nextDay.setUTCDate(nextDay.getUTCDate() + 1);
+      let nextDayDate = nextDay.toISOString().slice(0,10);
+      
+      history.push({ search : `date=${nextDayDate}`})
+    }
+
+    if(target.innerHTML === "Previous Day"){
+      let prevDay = new Date(dayDate);
+      prevDay.setUTCDate(prevDay.getUTCDate() - 1);
+      let prevDayDate = prevDay.toISOString().slice(0,10);
+
+      history.push({ search : `date=${prevDayDate}`})
+    }
+    
+    if(target.innerHTML === "Today"){
+      history.push('/');
+    }
+
+  }
+
   return (
-    <main className="text-center">
-      <h1 className="m-3">{formatDate(date)}</h1>
-      <button
-        onClick={() => setDate(previous(date))}
-        className="btn btn-sm btn-light"
-      >
-        Previous Day
-      </button>
-      <button
-        className="mx-3 btn btn-sm btn-light"
-        onClick={() => setDate(today())}
-      >
-        Today
-      </button>
-      <button
-        onClick={() => setDate(next(date))}
-        className="btn btn-sm btn-light"
-      >
-        Next Day
-      </button>
-      <br />
-      <label htmlFor="reservation_date" className="form-label m-3">
-        <input
-          type="date"
-          pattern="\d{4}-\d{2}-\d{2}"
-          name="reservation_date"
-          onChange={handleDateChange}
-          value={date}
-        />
-      </label>
-      <div className="d-md-flex mb-3 "></div>
+    <div>
+    <main>
+      <h1>Dashboard</h1>
+      <div className="d-md-flex mb-3">
+        <h4 className="mb-0">Reservations for {date}</h4>
+        <button onClick={handleClick}>
+            Previous Day
+        </button>
+        <button onClick={handleClick}>
+            Today
+        </button>
+        <button onClick={handleClick}>
+            Next Day
+        </button>
+      </div>
       <ErrorAlert error={reservationsError} />
+      <table className="table text-wrap text-center table-hover">
+          <thead className="thead-dark">
+            <tr className="text-center">
+              <th scope="col">ID</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Mobile Number</th>
+              <th scope="col">Date</th>
+              <th scope="col">Time</th>
+              <th scope="col">People</th>
+            </tr>
+          </thead>
+          <tbody>{reservationsRender()}</tbody>
+        </table>
+        <br />
+        <br />
+      <h4>Tables</h4>
       <ErrorAlert error={tablesError} />
-      <h3>Tables </h3>
-      <div className="d-flex justify-content-center mb-1 flex-wrap">
-        {tables.map((table) => (
-          <Tables key={table.table_id} table={table} />
-        ))}
-      </div>
-      {reservations.length ? (
-        <h3>Reservations</h3>
-      ) : (
-        `No reservations for ${date}`
-      )}
-      <div className="d-flex justify-content-center flex-wrap">
-        {reservations.map((reservation) => (
-          <Reservation
-            key={reservation.reservation_id}
-            reservation={reservation}
-          />
-        ))}
-      </div>
+      <table className="table">
+          <thead className="thead-dark">
+            <tr className="text-center">
+              <th scope="col">Table ID</th>
+              <th scope="col">Table Name</th>
+              <th scope="col">Capacity</th>
+              <th scope="col">Status</th>
+              <th scope="col">Reservation ID</th>
+              <th scope="col">Finish</th>
+            </tr>
+          </thead>
+          <tbody>{tablesRender()}</tbody>
+        </table>
     </main>
+    </div>
+
   );
 }
+
+export default Dashboard;
--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
+import Dashboard from "../dashboard/Dashboard";
+import ReservationsNew from "../reservations/ReservationsNew";
+import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
-import NotFound from "../Errors/NotFound";
-import ReservationForm from "../reservations/ReservationForm";
-import TableForm from "../Tables/TableForm";
-
-import Seating from "../dashboard/Seating";
-import Search from "../dashboard/Search"; 
-import Dashboard from "../dashboard/Dashboard"; 
+import useQuery from "../utils/useQuery";
+import TablesForm from "../tables/TablesForm";
+import { listReservations, listTables } from "../utils/api";
+import SeatReservation from "../reservations/SeatReservation";
+import Search from "../search/Search";
 
 /**
  * Defines all the routes for the application.
@@ -17,32 +18,74 @@ import Dashboard from "../dashboard/Dashboard";
  * @returns {JSX.Element}
  */
 function Routes() {
+  const [reservations, setReservations] = useState([]);
+  const [reservationsError, setReservationsError] = useState(null);
+  const [tables, setTables] = useState([]);
+  const [tablesError, setTablesError] = useState(null);
+
+  const query = useQuery();
+  const date = query.get("date") ? query.get("date") : today();
+
+  useEffect(loadDashboard, [date]);
+
+  // RENDER DASHBOARD
+
+  function loadDashboard() {
+    const abortController = new AbortController();
+    setReservationsError(null);
+    setTablesError(null);
+    listReservations({ date: date }, abortController.signal)
+      .then(setReservations)
+      .catch(setReservationsError);
+    listTables(abortController.signal)
+      .then((allTables) =>
+        allTables.sort((tableA, tableB) => tableA.table_id - tableB.table_id)
+      )
+      .then(setTables)
+      .catch(setTablesError);
+    return () => abortController.abort();
+  }
+
   return (
     <Switch>
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
+
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
       </Route>
-      <Route exact={true} path="/reservations/new">
-        <ReservationForm type="Create a New" />
+
+      <Route path="/reservations/new">
+        <ReservationsNew loadDashboard={loadDashboard} />
       </Route>
-      <Route path="/dashboard">
-        <Dashboard date={today()} />
-      </Route>
-     <Route path="/Tables/new">
-        <TableForm />
-      </Route>
-      <Route path="/reservations/:reservation_id/seat">
-        <Seating />
-      </Route>
+
       <Route path="/reservations/:reservation_id/edit">
-      <ReservationForm type="Edit" />
+        <ReservationsNew loadDashboard={loadDashboard} edit={true} />
       </Route>
+
+      <Route path="/reservations/:reservation_id/seat">
+        <SeatReservation loadDashboard={loadDashboard} tables={tables} />
+      </Route>
+      <Route exact={true} path="/tables/new">
+        <TablesForm loadDashboard={loadDashboard} />
+      </Route>
+
       <Route path="/search">
         <Search />
       </Route>
+
+      <Route path="/dashboard">
+        <Dashboard
+          date={date}
+          reservations={reservations}
+          reservationsError={reservationsError}
+          tables={tables}
+          tablesError={tablesError}
+          loadDashboard={loadDashboard}
+        />
+      </Route>
+
       <Route>
         <NotFound />
       </Route>
